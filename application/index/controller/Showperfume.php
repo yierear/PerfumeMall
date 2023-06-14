10b89680f7d90326ebfdec72abdbd5b369cae583
STingLeo <110530627+STingLeo@users.noreply.github.com>
2023-06-14 21:33:50 +0800

    左导航栏标签分类功能

--- application/index/controller/Showperfume.php
+++ application/index/controller/Showperfume.php
@@ -2,6 +2,7 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Db;
 use app\index\model\Perfume;
 use app\index\model\Shoplist;
 
@@ -17,7 +18,41 @@ class Showperfume extends Controller
     }
     
     public function clsperfume(){
+       $search="";
+               
+        if(!empty($_GET['pvalue'])){
+            $pname = $_GET['pname'];
+            $pvalue = $_GET['pvalue'];
+            
+            if($_GET['pname']==='pclass'){
+                $search.="pclass like '%$pvalue%'";
+            }
+            else if($_GET['pname']==='pclass1'){
+                $search.="pclass1 like '%$pvalue%'";
+            }
+            else if($_GET['pname']==='myclass'){
+                $search.="myclass like '%$pvalue%'";
+            }
+        }            
         
+        if(!empty($_GET['pvalue1'])){
+            $pname = $_GET['pname'];
+            $pvalue1 = $_GET['pvalue1'];
+            $pvalue2 = $_GET['pvalue2'];
+            
+            if($_GET['pname']==='yourprice'){
+                $search="yourprice between $pvalue1 and $pvalue2";
+            } else if($_GET['pname']==='SelledNum') {
+                $search="SelledNum between $pvalue1 and $pvalue2";
+            }
+        }
+        
+        
+        $data = Db::table('perfume')->where($search)->order('SelledNum','desc')->paginate(15);
+        $this->assign('result',$data);
+        $page=$data->render();
+        $this->assign('page',$page);
+        return $this->fetch();
     }
 }
 
